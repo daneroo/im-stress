ee1dd9e81832701fc033e66a6a25ac2f6bdea185
Daniel Lauzon <daniel.lauzon@gmail.com>
2014-04-29 14:21:44 -0400

    add thread and count to json

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var http = require('http');
 var numCPUs = require('os').cpus().length;
 numCPUs = Math.min(numCPUs,2);
 
+var total=0;
 if (cluster.isMaster) {
   // Fork workers.
   for (var i = 0; i < numCPUs; i++) {
@@ -23,7 +24,11 @@ if (cluster.isMaster) {
   // In this case its a HTTP server
   console.log('I am worker #' + cluster.worker.id);
   http.createServer(function(req, res) {
+    total+=1;
+    if (total%1000 == 0){
+      console.log(cluster.worker.id,'count',total);
+    }
     res.writeHead(200, {'Content-Type': 'application/json'});
-    res.end(JSON.stringify({now:new Date()}));
+    res.end(JSON.stringify({now:new Date(),thread:cluster.worker.id,total:total}));
   }).listen(8000);
 }
